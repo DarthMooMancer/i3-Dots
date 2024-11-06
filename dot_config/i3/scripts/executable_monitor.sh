e2381114b7050c027b2ccbd6b5af803ea2064839
DarthMooMancer <Haynes.a467@gmail.com>
2024-11-06 13:43:00 -0500

    Add .config/i3/config Add .config/i3/i3status.conf Add .config/i3/scripts/monitor.sh Add .config/i3/scripts/mouse.sh

---
+++ dot_config/i3/scripts/executable_monitor.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xrandr --output eDP --auto --output HDMI-A-0 --auto --right-of eDP0A
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 120.00
---
+++ dot_config/i3/scripts/executable_mouse.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xinput set-prop "8" "libinput Accel Profile Enabled" 0 1 0
+
