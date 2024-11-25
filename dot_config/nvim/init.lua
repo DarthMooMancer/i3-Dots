e52f368c8538a23a49d0c5e9e658453aabf1e17f
DarthMooMancer <Haynes.a467@gmail.com>
2024-11-24 21:45:11 -0500

    Update .config/i3/config Update .config/i3/scripts/i3blocks.conf Update .config/kitty/catppuccin_macchiato.conf Update .config/kitty/kitty.conf Update .config/nvim/init.lua Update .config/picom/picom.conf Update .zshrc

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,67 +1,21 @@
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin')
-
-local plugins = {
-  'windwp/nvim-autopairs',
-  'junegunn/fzf',
-  'neovim/nvim-lspconfig',
-  'nvim-treesitter/nvim-treesitter',
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'catppuccin/nvim',
-  'norcalli/nvim-colorizer.lua'
-}
-
-for _, plugin in pairs(plugins) do
-  Plug(plugin)
-end
-
-vim.call('plug#end')
-
-require("plugins")
-
-local options = {
-  completeopt = { "menuone", "noselect", "noinsert", "popup" },
-	shiftwidth = 2,
-  expandtab = true,
-	softtabstop = 2,
-	tabstop = 2,
-	wrap = false,
-	mouse = "",
-  number = true,
-  relativenumber = true,
-  termguicolors = true,
-	autoindent = true,
-  syntax = "enable",
-	smartindent = true,
-	scrolloff = 8,
-	swapfile = false,
-	backup = false,
-	hlsearch = false,
-	incsearch = true,
-}
-
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
-
-vim.g.mapleader = " "
+vim.opt.termguicolors = true
+require("config.lazy")
+require("colorizer").setup()
+vim.opt.completeopt = { "menuone", "noselect", "noinsert", "popup" }
+vim.opt.mouse = ""
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.relativenumber = true
+vim.opt.syntax = "enable"
+vim.opt.scrolloff = 8
+vim.opt.swapfile = false
+vim.opt.hlsearch = false
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-n>", vim.cmd.Ex)
-vim.keymap.set("n", "<C-p>", ":FZF<CR>")
-
-vim.cmd "set bg=dark"
-vim.cmd "colorscheme catppuccin-macchiato"
-vim.cmd "highlight Keyword gui=bold"
-vim.cmd "highlight Conditional gui=bold"
-vim.cmd "highlight Repeat gui=bold"
-vim.cmd "highlight Structure gui=bold,italic"
-vim.cmd "highlight Comment gui=italic"
--- vim.cmd "highlight Constant guifg=#999999"
-vim.cmd "highlight NormalFloat guibg=#333331"
--- vim.cmd "highlight Pmenu guifg=#FFFFFF guibg=#000000"
-vim.cmd "highlight Function gui=bold,italic"  --guifg=#707070
-vim.cmd "highlight StatusLine guibg=NONE"
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>")
